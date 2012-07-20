5d20caeb042eaf69862a18095dcf95cfcf2ce9d9
Ingo Schurr <ingo.schurr@kreuzwerker.de>
2012-07-20 20:59:38 +0200

    Add the necessary options

--- lib/mereport/cli.rb
+++ lib/mereport/cli.rb
@@ -1,11 +1,16 @@
 
 module MergeReport
   class CommandLine 
-    attr_accessor :login, :password, :user, :repository, :revision 
+    attr_accessor :login, :password, :user, :repository, :revision , :links
     attr_accessor :options  
     def initialize()
-      @options = {}
+      @options = {actions: [:merge, :commits]}
     end
+    
+    def [](key)
+      @options[key] || self.send(key)
+    end
+
     def self.parse(args)
       options = CommandLine.new
       opts = OptionParser.new do |opts|
@@ -40,6 +45,46 @@ module MergeReport
         opts.on("-D", "--depth DEPTH", "GitHub SHA to end with") do |depth|
           options.options[:depth] = depth.to_i
         end 
+
+        opts.on("-s", "--since TIME", "GitHub SHA to end with") do |time|
+          options.options[:until] = Time.parse(time)
+        end 
+
+        opts.on('-L', '--links LINKS', 'scans for LINKS') do |links|
+          options.links = links
+        end
+
+        opts.on( '-m', '--[no-]merge', "Negated forms" ) do |n|
+          if n
+            options.options[:actions] << :merge
+          else
+            options.options[:actions].delete(:merge)
+          end
+        end
+
+        opts.on( '-c', '--[no-]commits', "Negated forms" ) do |n|
+          if n
+            options.options[:actions] << :commits
+          else
+            options.options[:actions].delete(:commits)
+          end
+        end
+
+        opts.on( '-P', '--[no-]pull', "Negated forms" ) do |n|
+          if n
+            options.options[:actions] << :pull
+          else
+            options.options[:actions].delete(:pull)
+          end
+        end
+
+        opts.on( '-a', '--[no-]actions', "Negated forms" ) do |n|
+          if n
+            options.options[:actions] << :actions
+          else
+            options.options[:actions].delete(:actions)
+          end
+        end
 
       end
       opts.parse(args)
